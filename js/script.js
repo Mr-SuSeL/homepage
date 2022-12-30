b58ea589845d6edbf39b8fbe64fa18205b244bb4
Łukasz Susłowicz <susel.invest@gmail.com>
2022-12-30 15:23:35 +0100

    Function structure JS file

--- js/script.js
+++ js/script.js
@@ -1,26 +1,33 @@
-let button = document.querySelector(".button");
-let buttonReaction = document.querySelector(".button__reaction");
-var data = new Date();
-
-let godzina_teraz = data.getHours();
+{
 
 
-button.addEventListener("click", () => {
+const toggleContactInfo = () => {
+    const data = new Date();
+    const actualHour = data.getHours();
+    const buttonReaction = document.querySelector(".button__reaction");
 
-    if (godzina_teraz >= 15 && godzina_teraz <= 19) {
+    if (actualHour >= 15 && actualHour <= 19) {
         buttonReaction.innerHTML = "<p>To jest dobry czas na telefon:</p><p><ul><li><a href='tel:+48.888269988'>Telefon</a></li></ul></p>";
     }
-    else if (godzina_teraz > 19) {
+    else if (actualHour > 19) {
         buttonReaction.innerHTML = "<p>Po czasie, zatem napisz mejla</p><p><ul><li><a href='mailto: susel.invest@gmail.com'>E-mail</a></li></ul></p>";
     }
-    else if (godzina_teraz < 15) {
+    else if (actualHour < 15) {
         buttonReaction.innerHTML = "<p>Jestem w drugiej pracy teraz, zatem napisz mejla</p><p><ul><li><a href='mailto: susel.invest@gmail.com'>E-mail</a></li></ul></p><p>Lub poczekaj do godziny 15-tej</p>";
     }
     else {
         alert("Problem ze stroną lub serwerem");
-
     }
 
 
-})
+};
+
+const init = () => {
+    const button = document.querySelector(".button");
+    button.addEventListener("click", toggleContactInfo);
+};
+
+init();
+
+}
 
